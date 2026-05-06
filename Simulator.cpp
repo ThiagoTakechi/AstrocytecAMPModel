56e4f7211909a538f4e7c21afa623ddd4f92329d
Thiago Ohno Bezerra <87818787+ThiagoTakechi@users.noreply.github.com>
2026-05-06 01:29:07 -0300

    Update Simulator.cpp
    Updated author list and affiliations

--- Simulator.cpp
+++ Simulator.cpp
@@ -4,8 +4,8 @@
  *
  * Sijia Wang1,2†, Shilin Fang1†, Thiago O. Bezerra3†, Zhiyuan Wang1,2, Qimeng Zhao1,2, 
  * Xiaoou Wang1,2, Liang Wang4, Kui Wang1, Lijun Chen1,2, Funing Li1, Yuchen Gong1,2, 
- * Sha Li1,2, Jiesi Feng5, Yu Zheng5, Yulong Li5, Jun Chu4, Gerald M. Pao6, Antonio C. 
- * Roque3, Xufei Du1,2, Yu Mu1,2*
+ * Yongxin Yang1,2, Jiesi Feng5, Yu Zheng5, Yulong Li5, Jun Chu4, Gerald M. Pao6, Xufei 
+ * Du1,2, Jiu-lin Du1,2, Antonio C. Roque3, Yu Mu1,2*
  * 
  * Affiliations:
  * 
@@ -14,21 +14,21 @@
  * Chinese Academy of Sciences, 320 Yue-Yang Road, Shanghai 200031, China.
  * 
  * 2 University of Chinese Academy of Sciences,19A Yu-Quan Road, Beijing 100049, China.
- * 
+ *
  * 3 Department of Physics, School of Philosophy, Sciences and Letters of Ribeirão Preto, 
  * University of São Paulo, Ribeirão Preto, São Paulo, Brazil.
- * 
- * 4 Research Center for Primate Neuromodulation and Neuroimaging, Shenzhen Institutes of Advanced 
- * Technology, Chinese Academy of Sciences, Shenzhen 518055, China.
  *
- * 5 Academy for Advanced Interdisciplinary Studies, School of Psychological and Cognitive Sciences, 
- * Peking University, Beijing 100871, China. 
+ * 4 Research Center for Primate Neuromodulation and Neuroimaging, Shenzhen Institutes of 
+ * Advanced Technology, Chinese Academy of Sciences, Shenzhen 518055, China.
  *
- * 6 Okinawa Institute of Science and Technology Graduate University, Biological Nonlinear Dynamics 
- * Data Science Unit, Okinawa 904-0495, Japan.
+ * 5 Academy for Advanced Interdisciplinary Studies, School of Psychological and Cognitive 
+ * Sciences, Peking University, Beijing 100871, China.
  *
- * †These authors contributed equally to this work.
- * *Corresponding author. Email: my@ion.ac.cn
+ * 6 Okinawa Institute of Science and Technology Graduate University, Biological Nonlinear 
+ * Dynamics Data Science Unit, Okinawa 904-0495, Japan.
+ *
+ * † These authors contributed equally to this work.
+ * * Corresponding author. Email: my@ion.ac.cn
  *
  * This file provides the methods for running the simulation, interface for classes 
  * integration, and saving results.
